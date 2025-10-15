267a7b2af8df59f8dbf5bb1afb5e7ddbe95ae858
Hollow <deboerskelte@gmail.com>
2025-10-15 13:35:45 +0200

    door
    door handels and thingies

---
+++ VRProject1/Assets/Skelte's folder/FollowPhysics.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class FollowPhysics : MonoBehaviour
+{
+
+    public Transform target;
+    Rigidbody rb;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.MovePosition(target.transform.position);
+    }
+}
