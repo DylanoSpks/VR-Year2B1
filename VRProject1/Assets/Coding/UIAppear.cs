4356d056d18eae5a5a55952452f7e67a6a91b231
DylanoSpks <dylanored@yahoo.com>
2025-10-21 13:49:12 +0200

    UIText 2

--- VRProject1/Assets/Coding/UIAppear.cs
+++ VRProject1/Assets/Coding/UIAppear.cs
@@ -8,14 +8,15 @@ namespace Coding
     {
         [SerializeField] private GameObject offObject;
         [SerializeField] private GameObject uiTrigger;
+        [SerializeField] private GameObject uiTrigger2;
         [SerializeField] private GameObject player;
         [SerializeField] float remainingTime;
-
+        [SerializeField] float remainingTime2;
+        
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player") && remainingTime > 0)
                 StartCoroutine(StartTimer());
-
         }
     
         //Starts a timer that makes the hit box onenter and the UI disappear after a set amount of time
@@ -24,7 +25,12 @@ namespace Coding
             uiTrigger.SetActive(true);
             yield return new WaitForSeconds(remainingTime);
             uiTrigger.SetActive(false);
+            //After the first UI is done the 2nd start with its own timer that can be set within unity
+            uiTrigger2.SetActive(true);
+            yield return new WaitForSeconds(remainingTime2);
+            uiTrigger2.SetActive(false);
             offObject.SetActive(false);
         }
+        
     }
 }
