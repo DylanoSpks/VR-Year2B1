7e5035f918e9ebacd587c19cb22f827f5739334b
theotheslave <teodorsim@abv.bg>
2025-10-27 07:18:28 +0100

    fixed a bug with the interaction

--- VRProject1/Assets/Team Folder/Teo/Coding/ItemPickUp.cs
+++ VRProject1/Assets/Team Folder/Teo/Coding/ItemPickUp.cs
@@ -1,15 +1,15 @@
+﻿using System.Collections.Generic;
 using UnityEngine;
-using Oculus.Interaction; 
+using Oculus.Interaction;
 
-[RequireComponent(typeof(GrabInteractable))]
-public class FirstTimePickupItem_Meta : MonoBehaviour
+
+public class ItemPickUp : MonoBehaviour
 {
-    [SerializeField] private string itemId;                   // unique per item
-    [TextArea][SerializeField] private string dialogueText;  
+    [SerializeField] private string itemId;
+    [TextArea][SerializeField] private string dialogueText;
     private GrabInteractable _grab;
 
     private string Key => $"pickup_seen_{(string.IsNullOrEmpty(itemId) ? name : itemId)}";
-    private bool _subscribed;
 
     private void Awake()
     {
@@ -17,27 +17,11 @@ public class FirstTimePickupItem_Meta : MonoBehaviour
         if (string.IsNullOrEmpty(itemId)) itemId = gameObject.name;
     }
 
-    private void OnEnable()
-    {
-        if (_grab != null && !_subscribed)
-        {
-            _grab.WhenPointerEventRaised += OnPointerEvent;   // Meta Interaction SDK hook
-            _subscribed = true;
-        }
-    }
-
-    private void OnDisable()
-    {
-        if (_grab != null && _subscribed)
-        {
-            _grab.WhenPointerEventRaised -= OnPointerEvent;
-            _subscribed = false;
-        }
-    }
+    private void OnEnable() => _grab.WhenPointerEventRaised += OnPointerEvent;
+    private void OnDisable() => _grab.WhenPointerEventRaised -= OnPointerEvent;
 
     private void OnPointerEvent(PointerEvent evt)
     {
-        // Fire once, on first grab of this item
         if (evt.Type == PointerEventType.Select && PlayerPrefs.GetInt(Key, 0) == 0)
         {
             PlayerPrefs.SetInt(Key, 1);
